d2330239f32d721e63e86346a9c2dd23518c8d97
Sander Huijsen <sander.huijsen@gmail.com>
2021-08-26 15:15:07 +0800

    Fixed typo in comment.

--- blinkable.py
+++ blinkable.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class Blinkable(ABC):
     """
     We can use an Abstract Base Class (ABC) to create an interface.
-    The interface is an abstract method. It has not real implementation,
+    The interface is an abstract method. It has no real implementation,
     it can simply contain pass. It must, however, by imlemented
     by subclasses that promise to implement it.
     """
